34651edbd8759ba0277bb9a8e6ee78dc83c85b41
Bohdan Tsaplia <b.tsaplia@gmail.com>
2023-05-18 21:29:46 +0300

    some changes & build preparations

--- Assets/_Project/Scripts/World/ProceduralEnvironmentGenerator.cs
+++ Assets/_Project/Scripts/World/ProceduralEnvironmentGenerator.cs
@@ -9,7 +9,8 @@ namespace Gisha.HillClimb.World
     [ExecuteInEditMode]
     public class ProceduralEnvironmentGenerator : MonoBehaviour
     {
-        [SerializeField] private SpriteShapeController _shapeController;
+        [SerializeField] private SpriteShapeController shapeController;
+        [SerializeField] private SpriteShape[] spriteShapes;
 
         [SerializeField] private float levelLength = 30f;
         [SerializeField] private float xMultiplier = 2f;
@@ -42,28 +43,29 @@ namespace Gisha.HillClimb.World
 
         private void GenerateTerrain()
         {
-            _shapeController.spline.Clear();
+            shapeController.spline.Clear();
+            shapeController.spriteShape = spriteShapes[UnityEngine.Random.Range(0, spriteShapes.Length)];
+
             var seed = new Random().Next(500000);
-            
             for (int i = 0; i < levelLength; i++)
             {
                 float step = i * noiseStep + seed;
-                
+
                 _lastPos = transform.position +
                            new Vector3(i * xMultiplier, Mathf.PerlinNoise(0, step) * yMultiplier);
-                _shapeController.spline.InsertPointAt(i, _lastPos);
+                shapeController.spline.InsertPointAt(i, _lastPos);
 
                 if (i != 00 && Math.Abs(i - (levelLength - 1)) > 0.1f)
                 {
-                    _shapeController.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
-                    _shapeController.spline.SetLeftTangent(i, Vector3.left * xMultiplier * smoothness);
-                    _shapeController.spline.SetRightTangent(i, Vector3.right * xMultiplier * smoothness);
+                    shapeController.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
+                    shapeController.spline.SetLeftTangent(i, Vector3.left * xMultiplier * smoothness);
+                    shapeController.spline.SetRightTangent(i, Vector3.right * xMultiplier * smoothness);
                 }
             }
 
-            _shapeController.spline.InsertPointAt((int) levelLength,
+            shapeController.spline.InsertPointAt((int) levelLength,
                 new Vector3(_lastPos.x, transform.position.y - bottom));
-            _shapeController.spline.InsertPointAt((int) levelLength + 1,
+            shapeController.spline.InsertPointAt((int) levelLength + 1,
                 new Vector3(transform.position.x, transform.position.y - bottom));
         }
     }
